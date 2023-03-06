cb3db08a50095e5e51da83095f7753b4bf07ebc9
Zhaniya110 <zmeyramova@gmail.com>
2023-03-06 19:07:24 +0600

    created a game with words

--- test.php
+++ test.php
@@ -6,8 +6,9 @@
     }
 </style>
 <body>
-<h1 class="Title">Building a Mad Libs Game </h1>
+
     <form action="test.php" method="get">
+    <h1 class="Title">Building a Mad Libs Game </h1>
     <span style="color:#AFA;"> color: <input type="text" placeholder="color" class="Input-text" name="color">
     <span style="color:#AFA;"> flower: <input type="text" placeholder="flower" class="Input-text" name="flower">
     <span style="color:#AFA;"> someone: <input type="text" placeholder="someone" class="Input-text" name="someone"> <br>
