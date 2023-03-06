2d1323d82c787733a5c13bd858f658c107e1e4a9
Zhaniya110 <zmeyramova@gmail.com>
2023-03-06 18:53:06 +0600

    created a game with words

--- test.php
+++ test.php
@@ -1,4 +1,41 @@
-<?php
- Answer: echo $_GET["num1"] + $_GET["num2"];
+<html>
+<link rel="stylesheet" href="styke.css">
+<style>
+    p{color:white;
 
-?>
+    }
+</style>
+<body>
+    <form action="test.php" method="get">
+    <span style="color:#AFA;"> color: <input type="text" placeholder="color" class="Input-text" name="color">
+    <span style="color:#AFA;"> flower: <input type="text" placeholder="flower" class="Input-text" name="flower">
+    <span style="color:#AFA;"> someone: <input type="text" placeholder="someone" class="Input-text" name="someone"> <br>
+      <input type="submit" value="Submit" class="button">
+
+    </form>
+    <br><br>
+    <?php
+     $color = $_GET["color"];
+     $flower = $_GET["flower"];
+     $someone = $_GET["someone"];
+
+
+    echo "Roses are $color <br>"; 
+    echo "$flower are white <br>"; 
+    echo "I love $someone"
+    
+    
+    
+    ?>
+</body>
+
+
+
+
+
+
+
+
+
+
+</html>
