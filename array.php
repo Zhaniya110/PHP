58358835fdbf9c553c6bb095d7df0ed6a9500cd6
meiramovamariiam <meiramovamariiam@gmail.com>
2023-03-08 17:48:11 +0600

    Your message

--- array.php
+++ array.php
@@ -1,12 +1,31 @@
 <?php
 
 
-$my_array = array("apple", "banana", "orange");
+// $my_array = array("apple", "banana", "orange");
 
-var_dump($my_array);
-echo "<br>";
+// var_dump($my_array);
+// echo "<br>";
 
-echo "<br>";
+// echo "<br>";
 
-print_r($my_array);
+// print_r($my_array);
+
+
+
+$paper[] = "Copier";
+$paper[] = "Inkjet";
+$paper[] = "Laser";
+$paper[] = "Photo";
+
+
+for($i = 0; $i <4;++$i){
+    echo "$i: $paper[$i] <br>";
+}
+
+/*0: Copier
+1: Inkjet
+2: Laser
+3: Photo*/
+
 ?>
+
